8c932940f638fdd67d42ba2bd2aad9011fb2691f
Andreas Sorge <off@andreassorge.de>
2013-12-03 23:28:02 +0100

    Fix Qsub script

--- scripts/mm1q_qsub.sh
+++ scripts/mm1q_qsub.sh
@@ -10,5 +10,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # maximum number of jobs is 75,000
-qsub -cwd -V -q frigg.q,skadi.q -t 1-1001 -e /scratch02/sorge/mm1q/error -o /scratch02/sorge/mm1q/output $DIR/mm1q.sh 
+qsub -cwd -V -q frigg.q,skadi.q -t 1-10001 -e /scratch02/sorge/mm1q/error -o /scratch02/sorge/mm1q/output $DIR/mm1q.sh 
 
